12a5b7067f69ff0e973fc1fa55d18b593b082dde
stefano <s.siano@zerodo.it>
2021-07-22 12:25:17 +0200

    released 0.4.22 fixed ObservableF updates updated all libs

--- powerfulsama/src/main/java/com/stefanosiano/powerful_libraries/sama/utils/UtilClasses.kt
+++ powerfulsama/src/main/java/com/stefanosiano/powerful_libraries/sama/utils/UtilClasses.kt
@@ -2,10 +2,11 @@ package com.stefanosiano.powerful_libraries.sama.utils
 
 import androidx.databinding.ObservableField
 import java.lang.ref.WeakReference
+import java.math.BigDecimal
 
 /** Returns a pair made up of weakReferences */
 open class WeakPair<T, R>(first: T, second: R) {
-    val p = Pair(WeakReference(first), WeakReference(second))
+    private val p = Pair(WeakReference(first), WeakReference(second))
 
     /** Return first item */
     fun first() : T? = p.first.get()
@@ -17,8 +18,14 @@ open class WeakPair<T, R>(first: T, second: R) {
     fun clear() { p.first.clear(); p.second.clear() }
 }
 
-/** An [ObservableField] with non-nullable values */
-class ObservableF<T>(value: T) : ObservableField<T>(value) {
+/** An [ObservableField] with non-nullable values. You can specify to update the value with [onlyWhen] function.
+ * By default the new value will be set only when it's different from old (via != comparison). Supports [BigDecimal], too */
+class ObservableF<T>(value: T, onlyWhen: ((old: T, new: T) -> Boolean)? = null) : ObservableField<T>(value) {
+    private val setOnlyWhen: (old: T, new: T) -> Boolean = onlyWhen ?: { old: T, new: T -> when {
+        new is BigDecimal -> (old as BigDecimal).toDouble() != new.toDouble()
+        else -> old != new
+    } }
+
     override fun get(): T = super.get()!!
-    override fun set(value: T) = super.set(value)
+    override fun set(value: T) { if(setOnlyWhen(get(), value)) super.set(value) }
 }
