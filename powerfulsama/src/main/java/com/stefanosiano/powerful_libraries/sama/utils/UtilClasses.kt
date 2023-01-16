0157bc4a7ea1a5a4a4992716562be3d68f5f05fb
stefanosiano <stefano.siano91@gmail.com>
2023-01-16 16:53:26 +0100

    fixed all detekt issues added makefile and github actions releasing 0.5.00

--- powerfulsama/src/main/java/com/stefanosiano/powerful_libraries/sama/utils/UtilClasses.kt
+++ powerfulsama/src/main/java/com/stefanosiano/powerful_libraries/sama/utils/UtilClasses.kt
@@ -9,17 +9,21 @@ open class WeakPair<T, R>(first: T, second: R) {
     private val p = Pair(WeakReference(first), WeakReference(second))
 
     /** Return first item. */
-    fun first() : T? = p.first.get()
+    fun first(): T? = p.first.get()
 
     /** Return second item. */
-    fun second() : R? = p.second.get()
+    fun second(): R? = p.second.get()
 
     /** Clear internal weakReferences. */
-    fun clear() { p.first.clear(); p.second.clear() }
+    fun clear() {
+        p.first.clear()
+        p.second.clear()
+    }
 }
 
 /**
- * An [ObservableField] with non-nullable values. You can specify to update the value with [onlyWhen] function.
+ * An [ObservableField] with non-nullable values.
+ * You can specify to update the value with [onlyWhen] function.
  * By default the new value will be set only when it's different from old (via != comparison).
  * Supports [BigDecimal], too.
  */
@@ -27,15 +31,14 @@ class ObservableF<T>(value: T, onlyWhen: ((old: T, new: T) -> Boolean)? = null) 
     private val setOnlyWhen: (old: T, new: T) -> Boolean = onlyWhen ?: { old: T, new: T ->
         if (new is BigDecimal) {
             (old as BigDecimal).toDouble() != new.toDouble()
-        }
-        else {
+        } else {
             old != new
         }
     }
 
     override fun get(): T = super.get()!!
     override fun set(value: T) {
-        if(setOnlyWhen(get(), value)) {
+        if (setOnlyWhen(get(), value)) {
             super.set(value)
         }
     }
