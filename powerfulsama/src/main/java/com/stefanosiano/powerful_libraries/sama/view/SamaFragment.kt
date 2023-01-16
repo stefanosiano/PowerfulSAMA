488098da993557d02de6193c222e7efa2e730a5e
stefanosiano <stefano.siano91@gmail.com>
2023-01-16 16:57:38 +0100

    fixed all detekt issues added makefile and github actions releasing 0.5.00

--- powerfulsama/src/main/java/com/stefanosiano/powerful_libraries/sama/view/SamaFragment.kt
+++ powerfulsama/src/main/java/com/stefanosiano/powerful_libraries/sama/view/SamaFragment.kt
@@ -2,16 +2,16 @@ package com.stefanosiano.powerful_libraries.sama.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.stefanosiano.powerful_libraries.sama.extensions.coroutineSamaHandler
-import com.stefanosiano.powerful_libraries.sama.extensions.logVerbose
-import com.stefanosiano.powerful_libraries.sama.extensions.tryOrNull
+import com.stefanosiano.powerful_libraries.sama.coroutineSamaHandler
+import com.stefanosiano.powerful_libraries.sama.logVerbose
+import com.stefanosiano.powerful_libraries.sama.tryOrNull
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 
 /** Abstract Fragment for all Fragments to extend. */
-abstract class SamaFragment: Fragment(), CoroutineScope {
+abstract class SamaFragment : Fragment(), CoroutineScope {
 
     internal var logTag: String? = null
 
@@ -49,12 +49,16 @@ abstract class SamaFragment: Fragment(), CoroutineScope {
         coroutineContext.cancel()
     }
 
-    /** Used by extending classes to clear references used by the fragment (does nothing by default). */
+    /**
+     * Used by extending classes to programmatically clear references used by the fragment.
+     * Does nothing by default.
+     */
     open fun clear() {}
 
     /** Removes this fragment from the stack of the fragment manager. */
     fun remove() {
-        tryOrNull { fragmentManager?.beginTransaction()?.remove(this)?.commit() }
+        tryOrNull {
+            fragmentManager?.beginTransaction()?.remove(this)?.commit()
+        }
     }
-
 }
